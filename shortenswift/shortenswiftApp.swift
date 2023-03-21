c7e665dce0e690ad24528c65592fd8fd80312096
aliirz <dejavu87@gmail.com>
2023-03-21 20:40:44 +0500

    Initial Commit

---
+++ shortenswift/shortenswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  shortenswiftApp.swift
+//  shortenswift
+//
+//  Created by Ali Raza on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct shortenswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
